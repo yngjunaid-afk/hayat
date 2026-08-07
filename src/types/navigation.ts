74c1a868691e5ae66525d06fbcdc3ff2a9ed6223
yngjunaid-afk <yng.junaid@gmail.com>
2026-08-07 17:58:19 +0530

    Modified by www.SourceFiles.app

---
+++ src/types/navigation.ts
@@ -0,0 +1,14 @@
+export type RootStackParamList = {
+  Splash: undefined;
+  Login: undefined;
+  Register: undefined;
+  MainTabs: undefined;
+  Profile: undefined;
+  Player: {
+    songId: string;
+  };
+  Playlist: {
+    playlistId: string;
+  };
+  Room: undefined;
+};
