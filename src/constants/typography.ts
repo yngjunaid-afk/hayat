5c82ca5867b9c39b7f3e4b6a95ee0251b4401f4f
yngjunaid-afk <yng.junaid@gmail.com>
2026-08-07 17:53:03 +0530

    Modified by www.SourceFiles.app

---
+++ src/constants/typography.ts
@@ -0,0 +1,31 @@
+export const TYPOGRAPHY = {
+  logo: {
+    fontSize: 42,
+    fontWeight: "700" as const,
+  },
+
+  heading: {
+    fontSize: 28,
+    fontWeight: "700" as const,
+  },
+
+  subHeading: {
+    fontSize: 20,
+    fontWeight: "600" as const,
+  },
+
+  body: {
+    fontSize: 16,
+    fontWeight: "400" as const,
+  },
+
+  caption: {
+    fontSize: 14,
+    fontWeight: "400" as const,
+  },
+
+  small: {
+    fontSize: 12,
+    fontWeight: "400" as const,
+  },
+};
